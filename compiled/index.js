58eb28b304047e8ed31875b8f4fcf3b52a2b3e84
Alexandr Mozolevsky <alexandr.mozolevsky@itibo.com>
2016-06-28 16:37:41 +0300

    Added compiled files

---
+++ compiled/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+  res.sendfile('index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.on('established', function (msg) {
+    console.log('User says:', msg);
+    io.emit('getusername', { userid: Math.random() * 1000 });
+  });
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function () {
+  console.log('listening on port 3000...');
+});
